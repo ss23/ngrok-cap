583bf89623da94bfeb701ca17cdec8dd14137412
ss23 <ss23@ss23.geek.nz>
2019-04-22 15:42:12 +0000

    Add first implmentation of thread-per-IP
    Doesn't work properly as of yet due to IPv6 issues with ngrok

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"net"
 	"net/http"
 	"os/exec"
 	"sync"
@@ -16,11 +17,13 @@ func main() {
 	var start = flag.Int("start", 0, "Begin scanning from address 0")
 	var step = flag.Int("step", 1, "Step this much each time - used for distributed execution")
 	var randomize = flag.Bool("random", false, "Randomize the start step")
-	var threads = flag.Int("threads", 1, "Processing threads")
+	var threads = flag.Int("threads", 1, "Processing threads per IP address")
 	flag.Parse()
 
+	// We sometimes need the random number generator seeded, even if not picking a random Start
+	rand.Seed(time.Now().UnixNano())
+
 	if *randomize {
-		rand.Seed(time.Now().UnixNano())
 		*start = rand.Int()
 	}
 
@@ -39,9 +42,15 @@ func main() {
 	// WaitGroup for checkHosts workers (the only threaded component of the system)
 	var wg sync.WaitGroup
 
-	wg.Add(*threads)
-	for i := 0; i < *threads; i++ {
-		go checkHosts(hosts, validHosts, &wg, &stats)
+	// Get our addresses
+	addrs := getLocalAddresses()
+
+	wg.Add(*threads * len(addrs))
+
+	for _, addr := range addrs {
+		for i := 0; i < *threads; i++ {
+			go checkHosts(hosts, validHosts, &wg, &stats, addr)
+		}
 	}
 
 	// TODO: Instead of creating a new set of network connections, we should just render the response we already got
@@ -72,12 +81,16 @@ func generateHosts(hosts chan<- string, start int, step int) {
 	close(hosts)
 }
 
-func checkHosts(hosts <-chan string, validHosts chan<- string, wg *sync.WaitGroup, stats *Stats) {
+func checkHosts(hosts <-chan string, validHosts chan<- string, wg *sync.WaitGroup, stats *Stats, addr *net.IPNet) {
 	for host := range hosts {
+		// For each host, we need to get a new dialier with a new local address
+		client := GetHttpClient(addr)
 		// Test if the ngrok.io URL returns the right response
 		// Use HEAD requests to save bandwidth
-		resp, err := http.Head("http://" + host + ".ngrok.io/")
+		resp, err := client.Head("http://" + host + ".ngrok.io/")
 		if err != nil {
+			// Assume this was a timeout
+			// TODO: Fix this -- re-add it to the queue
 			panic(err)
 		}
 		// Categorize it based on the response
@@ -87,7 +100,7 @@ func checkHosts(hosts <-chan string, validHosts chan<- string, wg *sync.WaitGrou
 		} else if resp.StatusCode == 502 && resp.ContentLength == 1590 {
 			// This occurs when the ngrok client is running, but the port on the client end is not open
 			stats.Increment("tunneldown")
-		/* } else if strings.Contains(bodyStr, "This tunnel expired ") {
+			/* } else if strings.Contains(bodyStr, "This tunnel expired ") {
 			// Free tunnels cannot be up for a long period, else they expire
 			// This tunnel expired x days ago
 			stats.Increment("expired")
@@ -98,6 +111,12 @@ func checkHosts(hosts <-chan string, validHosts chan<- string, wg *sync.WaitGrou
 			// Submit for a screenshot
 			validHosts <- host
 			stats.Increment("valid")
+		}
+		// Close out connection(s)
+		//client.CloseIdleConnections()
+		transport, ok := client.Transport.(*http.Transport)
+		if ok {
+			transport.CloseIdleConnections()
 		}
 	}
 	wg.Done()
@@ -113,7 +132,7 @@ func screenshotHosts(validHosts <-chan string) chan bool {
 			cmd := exec.Command("node",
 				"./http-screenshotter.js",
 				"/home/ubuntu/images/",
-				"http", validHost + ".ngrok.io", "80",
+				"http", validHost+".ngrok.io", "80",
 			)
 			timer := time.AfterFunc(30*time.Second, func() {
 				fmt.Println("Killing stalled Chrome screenshot")
@@ -174,4 +193,77 @@ func showStatus(s *Stats) {
 		// Only update once per second
 		time.Sleep(1 * time.Second)
 	}
+}
+
+// Get local addresses
+func getLocalAddresses() []*net.IPNet {
+	validAddr := make([]*net.IPNet, 0)
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			panic(err)
+		}
+
+		for _, a := range addrs {
+			switch v := a.(type) {
+			// TODO: When is this IPAddr instead of IPNet?
+			case *net.IPNet:
+				// Valid IP address here, need to check if it's local or not though
+				if isPrivateIP(v.IP) {
+					continue
+				}
+				// It's not local, so lets add it to the list of IPs we'll use to enumerate valid tunnels
+				validAddr = append(validAddr, v)
+			}
+		}
+
+	}
+
+	return validAddr
+}
+
+func isPrivateIP(ip net.IP) bool {
+	privateIPBlocks := make([]*net.IPNet, 0)
+
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+func GetHttpClient(addr *net.IPNet) http.Client {
+	dialer := &net.Dialer{
+		Timeout:   2 * time.Second,
+		KeepAlive: 0,
+		LocalAddr: &net.TCPAddr{
+			IP:   addr.IP,
+			Port: 0,
+		},
+	}
+	tr := &http.Transport{
+		Dial:                dialer.Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	fmt.Println(addr)
+	client := http.Client{Transport: tr}
+	return client
 }
